008b9ab247c067ebb99a2be5074915d1d1a209a3
r-nl <rnelsomain@gmail.com>
2023-12-18 23:53:48 -0700

    Update Title

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,6 @@
+<svelte:head>
+	<title>Robert Nelson | Fullstack Developer, UI/UX Designer</title>
+</svelte:head>
+
 <h1 class="text-xl">Sveltekit spinup time</h1>
 <p>The hour of portfolios is upon us</p>
