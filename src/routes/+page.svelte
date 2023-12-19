d304be9abe42ffa5e63b46b4f09cdb953e43e0b2
r-nl <rnelsomain@gmail.com>
2023-12-18 23:42:37 -0700

    Basic layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,2 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1 class="text-xl">Sveltekit spinup time</h1>
+<p>The hour of portfolios is upon us</p>
