ed96cbfb0979e3ed39cec2d1843353c47583890e
r-nl <rnelsomain@gmail.com>
2024-01-28 00:07:05 -0700

    Vercel Analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,11 @@
 <script>
 	import NavigationBar from '$lib/components/NavigationBar.svelte';
 	import '../app.css';
+	import { dev } from '$app/environment';
+	import { inject } from '@vercel/analytics';
+
+	//Activate Vercel Analytics
+	inject({ mode: dev ? 'development' : 'production' });
 </script>
 
 <div
