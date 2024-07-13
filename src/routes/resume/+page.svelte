665c12b9a1f9094cfde135fba034ad9c89fbae9f
r-nl <rob@r-nl.dev>
2024-07-13 16:02:23 -0600

    fix margin

--- src/routes/resume/+page.svelte
+++ src/routes/resume/+page.svelte
@@ -4,7 +4,7 @@
 </script>
 
 <div class="min-w-full min-h-full bg-slate-50 flex justify-center">
-	<div class="prose prose-sm prose-slate font-inter py-16">
+	<div class="prose prose-sm prose-slate font-inter py-16 mx-7">
 		<ResumeMarkdown />
 	</div>
 </div>
